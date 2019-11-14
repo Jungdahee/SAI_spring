bee2b5a73452b788405e90a505edfeda359241c5
jungdahee <jungdahee010@naver.com>
2019-11-14 17:33:54 +0900

    19.11.14 feat: Add SubwayServiceImpl.java

---
+++ src/main/java/com/tol/sai/service/SubwayServiecImpl.java
@@ -0,0 +1,24 @@
+package com.tol.sai.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+ 
+import org.springframework.stereotype.Service;
+ 
+import com.tol.sai.dao.SubwayDAO;
+import com.tol.sai.dto.SubwayVO;
+
+@Service
+public class SubwayServiecImpl implements SubwayService{
+	
+	@Inject
+	private SubwayDAO dao;
+
+	@Override
+	public List<SubwayVO> searchLocation() throws Exception {
+		// TODO Auto-generated method stub
+		return dao.searchLocation();
+	}
+	
+}
