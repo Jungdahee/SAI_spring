9ea120c689aa4753730c707977a779c8ede65481
jungdahee <jungdahee010@naver.com>
2019-11-14 16:16:00 +0900

    19.11.14 feat: Add SubwayVO.java

---
+++ src/main/java/com/tol/sai/dto/SubwayVO.java
@@ -0,0 +1,78 @@
+package com.tol.sai.dto;
+
+public class SubwayVO {
+	
+	private int stationId;
+	private String stationName;
+	private int subwayId;
+	private String subwayName;
+	private double tmX;
+	private double tmY;
+	private double wgsX;
+	private double wgsY;
+	
+	public int getStationId() {
+		return stationId;
+	}
+	
+	public void setStationId(int stationId) {
+		this.stationId = stationId;
+	}
+	
+	public String getStationName() {
+		return stationName;
+	}
+	
+	public void setStationName(String stationName) {
+		this.stationName = stationName;
+	}
+	
+	public int getSubwayId() {
+		return subwayId;
+	}
+	
+	public void setSubwayId(int subwayId) {
+		this.subwayId = subwayId;
+	}
+	
+	public String getSubwayName() {
+		return subwayName;
+	}
+	
+	public void setSubwayName(String subwayName) {
+		this.subwayName = subwayName;
+	}
+	
+	public double getTmX() {
+		return tmX;
+	}
+	
+	public void setTmX(double tmX) {
+		this.tmX = tmX;
+	}
+	
+	public double getTmY() {
+		return tmY;
+	}
+	
+	public void setTmY(double tmY) {
+		this.tmY = tmY;
+	}
+	
+	public double getWgsX() {
+		return wgsX;
+	}
+	
+	public void setWgsX(double wgsX) {
+		this.wgsX = wgsX;
+	}
+	
+	public double getWgsY() {
+		return wgsY;
+	}
+	
+	public void setWgsY(double wgsY) {
+		this.wgsY = wgsY;
+	}
+	
+}
