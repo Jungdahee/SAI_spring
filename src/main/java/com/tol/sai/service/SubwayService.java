fb5ca1fc871ab946d8d91e1b90a030aab5563c0e
jungdahee <jungdahee010@naver.com>
2019-11-21 23:37:27 +0900

    19.11.21 refactor: Delete searchCentricPoint method

--- src/main/java/com/tol/sai/service/SubwayService.java
+++ src/main/java/com/tol/sai/service/SubwayService.java
@@ -7,13 +7,10 @@ import com.tol.sai.dto.SubwayVO;
 
 public interface SubwayService {
 	
-	//무게 중심 좌표 반환 메소드
-	public String[] searchCentricPoint(String people[]) throws Exception;
-	
 	//지하철 이름 -> WGS 좌표 반환 메소드
 	public List<SubwayVO> searchLocation(String stationName) throws Exception;
 	
-	public String[] calculateLocation(List<SubwayVO> subwayInfoList) throws Exception;
+	public Double[] calculateLocation(List<SubwayVO> subwayInfoList) throws Exception;
 
 	public String[] searchTenSubway(String centricLocation[]) throws Exception;
 
